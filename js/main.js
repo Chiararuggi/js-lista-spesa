8e5860ea5f8a0abcfb9de590c7ad5a2ce809e499
chiara.ruggi <chiara.ruggi1998@gmail.com>
2023-11-05 19:25:16 +0100

    while doesnt work

---
+++ js/main.js
@@ -0,0 +1,15 @@
+let shoppingList = [
+    "cereali",
+    "pasta",
+    "frutta",
+    "verdure",
+    "biscotti"
+];
+
+console.log(shoppingList);
+
+let i = 0;
+while(i < shoppingList.lenght){
+    console.log(shoppingList[i]);
+    i++;
+}
